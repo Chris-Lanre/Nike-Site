2a176345689d3a709dfd54e4b77463e1f447493e
Chris-Lanre <121674000+Chris-Lanre@users.noreply.github.com>
2023-11-15 04:13:23 -0800

    Add files via upload

---
+++ src/Sections/index.js
@@ -0,0 +1,19 @@
+import Hero  from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    Subscribe,
+    SpecialOffers,
+    SuperQuality,
+    CustomerReviews,
+    Footer,
+}
